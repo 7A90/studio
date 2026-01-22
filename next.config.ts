15d597a39a418f11b1faace354e2992d1c19c516
KNOW MORE <hakouhakoo@gmail.com>
2026-01-22 19:10:36 +0000

    done next.congif.ts src/lib/placeholder-images.json

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'images..com',
         port: '',
         pathname: '/**',
       },
