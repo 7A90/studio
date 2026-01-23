5431eae087579405dc98fa4ad6bb68a6238ec39c
KNOW MORE <hakouhakoo@gmail.com>
2026-01-23 11:01:23 +0000

    upd

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yddbgayalsebkjutjnif.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZGJnYXlhbHNlYmtqdXRqbmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0OTI4ODcsImV4cCI6MjAzMjA2ODg4N30.6-h13sv05blBguTlbWAdxQ_N195vEMl-M9fprJ9_MAQ'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
