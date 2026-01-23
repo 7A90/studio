fcf320b6e5bdad94148cef9a30f43b029b912fbe
KNOW MORE <hakouhakoo@gmail.com>
2026-01-23 10:50:43 +0000

    clieb

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = https://yddbgayalsebkjutjnif.supabase.co
 const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZGJnYXlhbHNlYmtqdXRqbmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0OTI4ODcsImV4cCI6MjAzMjA2ODg4N30.6-h13sv05blBguTlbWAdxQ_N195vEMl-M9fprJ9_MAQ
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(https://yddbgayalsebkjutjnif.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZGJnYXlhbHNlYmtqdXRqbmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0OTI4ODcsImV4cCI6MjAzMjA2ODg4N30.6-h13sv05blBguTlbWAdxQ_N195vEMl-M9fprJ9_MAQ)
